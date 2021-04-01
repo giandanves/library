bd6103a697ea0524806e196325b18ba2f0ada7c4
Giancarlo Danves <gdanves@gmail.com>
2021-03-31 22:25:13 -0300

    instantiable library created based on factory functions

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const Book = (title, author, numberOfPages, read) => {
 
     const info = () => {
         console.log({
@@ -6,69 +7,36 @@
 
     }
 
-
-const libraryOne = new Library();
-
-function Book(title, author, numberOfPages, read) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
+    return { title, author, numberOfPages, read, info }
 
 }
 
+const Library = () => {
+    let books = [];
 
-Library.prototype.addBook = function (title, author, numberOfPages, read) {
-    const book = new Book(title, author, numberOfPages, read);
-    if (title == undefined || author == undefined || numberOfPages == undefined || read == undefined) {
-        throw new Error('Error! Insufficient data!')
+    const addBook = book => {
+        books.push(book);
     }
-    else {
-        this.books.push(book);
+
+    const findBook = title => {
+        return books.find(book => book.title === title)
     }
-}
 
-
-Library.prototype.findBook = function (title) {
-
-    return this.books.find(function (book) {
-        return book.title === title
-    })
-
-    //for (let i in this.books) {
-    //  if (this.books[i].title == title) {
-    //    return this.books[i];
-    //}
-    //}
-}
-
-Library.prototype.removeBook = function (title) {
-    this.books = this.books.filter(function (book) {
-        return (book.title !== title)
-    })
-
-
-    //const i = this.books.findIndex(function (book) {
-    //return book.title === title
-    //})
-
-    //for (let i in this.books) {
-    //if (this.books[i].title == title) {
-    //this.books.splice(i, 1);
-    //}
-    //}
-
-}
-
-
-Book.prototype.info = function () {
-    let info = '';
-
-    for (let prop in this) {
-        let isOwn = this.hasOwnProperty(prop);
-        if (isOwn) {
-            info += (`${this[prop]}. `);
-        }
+    const findBookIndex = title => {
+        return books.findIndex(book => book.title === title)
     }
-    return info;
+
+    const removeBook = title => {
+        const i = findBookIndex(title);
+        books.splice(i, 1);
+    }
+
+    const showBooks = () => {
+        const showBook = books.slice();
+        return showBook;
+    }
+
+    return { addBook, findBook, removeBook, showBooks }
 }
+
+
