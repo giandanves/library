e50153abc5b0d48be114428ad5b12743cbf9c2df
Giancarlo Danves <gdanves@gmail.com>
2021-04-05 08:55:11 -0300

    library with classes

--- main.js
+++ main.js
@@ -1,71 +1,47 @@
 
-function Library() {
-    this.books = new Array();
-}
+class Library {
 
-
-const libraryOne = new Library();
-
-function Book(title, author, numberOfPages, read) {
-    this.title = title;
-    this.author = author;
-    this.numberOfPages = numberOfPages;
-    this.read = read;
-
-}
-
-
-Library.prototype.addBook = function (title, author, numberOfPages, read) {
-    const book = new Book(title, author, numberOfPages, read);
-    if (title == undefined || author == undefined || numberOfPages == undefined || read == undefined) {
-        throw new Error('Error! Insufficient data!')
+    constructor(books) {
+        this.books = new Array();
     }
-    else {
+
+    addBook(book) {
         this.books.push(book);
     }
-}
 
-
-Library.prototype.findBook = function (title) {
-
-    return this.books.find(function (book) {
-        return book.title === title
-    })
-
-    //for (let i in this.books) {
-    //  if (this.books[i].title == title) {
-    //    return this.books[i];
-    //}
-    //}
-}
-
-Library.prototype.removeBook = function (title) {
-    this.books = this.books.filter(function (book) {
-        return (book.title !== title)
-    })
-
-
-    //const i = this.books.findIndex(function (book) {
-    //return book.title === title
-    //})
-
-    //for (let i in this.books) {
-    //if (this.books[i].title == title) {
-    //this.books.splice(i, 1);
-    //}
-    //}
-
-}
-
-
-Book.prototype.info = function () {
-    let info = '';
-
-    for (let prop in this) {
-        let isOwn = this.hasOwnProperty(prop);
-        if (isOwn) {
-            info += (`${this[prop]}. `);
-        }
+    findBook(title) {
+        return this.books.find(function (book) {
+            return book.title === title
+        })
     }
-    return info;
+
+    removeBook(title) {
+        this.books = this.books.filter(function (book) {
+            return (book.title !== title)
+        })
+
+    }
+
+    get showBooks() {
+        return this.books;
+    }
+
 }
+
+class Book {
+
+    constructor(title, author, numberOfPages, read) {
+        this.title = title;
+        this.author = author;
+        this.numberOfPages = numberOfPages;
+        this.read = read;
+    }
+
+    get info() {
+        console.log(
+            this.title, this.author, this.numberOfPages, this.read
+        )
+    }
+}
+
+
